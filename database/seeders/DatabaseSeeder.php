b7656a72becbda3d435f7d13b8e8a4ad704fb94c
flasky06 <bonnienjuguna106@gmail.com>
2024-09-20 19:28:43 +0300

    created db modes and seeded db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Project;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Factories\ProjectFactory;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +18,15 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Bonface',
+            'email' => 'bonnienjuguna106@gmail.com',
+            'password'=>bcrypt('123.321A'),
+            'email_verified_at'=>time()
         ]);
+
+        Project::factory()
+        ->count(30)
+        ->hasTasks(30)
+        ->create();
     }
 }
